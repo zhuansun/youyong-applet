873572d22ef5057faa6f7624402d8fd33a32c90c
颛孙鹏程 <zhuansunpengcheng@qq.com>
2019-05-22 17:08:15 +0800

    update : 添加登陆页面

--- app.js
+++ app.js
@@ -3,11 +3,6 @@ import urlConfig from './etc/config'
 //app.js
 App({
 
-  //为了确保token被捕获之后，不会恶意请求。 需要在生成token的时候，加上ip吧。
-  //只有通过小程序的App.onLaunch才可以生成token
-  //加上ip限制的话，就会有一个问题，如果用户在使用期间切换了网络，需要提醒用户： 检测到您的网络发生变化，为了您的安全，请重新登陆
-
-
   onLaunch: function () {
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
@@ -28,29 +23,7 @@ App({
     })
 
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-        wx.request({
-          url: urlConfig.basePath +'/api/user/sign/in', // 仅为示例，并非真实的接口地址
-          data: {
-            code: res.code
-          },
-          method: "POST",
-          header: {
-            'content-type': 'application/json' // 默认值
-          },
-          success(res) {
-            if(res.code == 200){
-              console.log("success");
-            }else{
-              console.log("fail");
-            }
-          }
-        })
-      }
-    })
+   
     // 获取用户信息
     wx.getSetting({
       success: res => {
@@ -58,14 +31,8 @@ App({
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
             success: res => {
-
-              console.log("-----"+res)
-              console.log(res)
-
-
               // 可以将 res 发送给后台解码出 unionId
               this.globalData.userInfo = res.userInfo
-
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
               if (this.userInfoReadyCallback) {
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,49 +1,97 @@
+import urlConfig from '../../etc/config'
+const {
+  $Message
+} = require('../../dist/base/index');
 //index.js
 //获取应用实例
 const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    current: 0,
+    hasLogin: false
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+
+  toUse: function (e) {
+    console.log("跳转到首页，准备使用了。。。。。。");
+  },
+
+  login: function(e) {
+    //拿到用户的头像信息之后，紧接着请求服务器接口进行登陆
+    wx.login({
+      success: res => {
+        const _this = this;
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        wx.request({
+          url: urlConfig.basePath + '/api/user/sign/in',
+          data: {
+            code: res.code
+          },
+          method: "POST",
+          header: {
+            'content-type': 'application/json' // 默认值
+          },
+          success(res) {
+            console.log(res.data.code);
+            if (res.data.code == 200) {
+              console.log("success");
+              $Message({
+                content: "登陆成功",
+                type: 'success'
+              });
+              _this.setData({
+                current: 2,
+                hasLogin: true
+              })
+            } else {
+              $Message({
+                content: res.data.msg,
+                type: 'error'
+              });
+            }
+          }
+        })
+      }
     })
   },
+
+
   onLoad: function() {
+    //1.判断是否有用户的头像信息，2.判断缓存中是否有token
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
-        hasUserInfo: true
+        hasUserInfo: true,
+        current: 1
       })
     } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
         this.setData({
           userInfo: res.userInfo,
-          hasUserInfo: true
+          hasUserInfo: true,
+          current: 1
         })
       }
     } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
       wx.getUserInfo({
         success: res => {
+          console.log(res.userInfo);
           app.globalData.userInfo = res.userInfo
           this.setData({
             userInfo: res.userInfo,
-            hasUserInfo: true
+            hasUserInfo: true,
+            current: 1
           })
         }
       })
     }
   },
-  login: function(e) {
+
+
+  getUserAvatarAndName: function(e) {
     console.log(e)
     console.log(app.urlConfig.basePath)
     console.log(e.detail.userInfo)
@@ -53,27 +101,13 @@ Page({
       app.globalData.userInfo = e.detail.userInfo
       this.setData({
         userInfo: e.detail.userInfo,
-        hasUserInfo: true
+        hasUserInfo: true,
+        current: 1
       })
-      //拿到用户的头像信息之后，紧接着请求服务器接口进行登陆
-      // wx.login({
-      //   success(res) {
-      //     if (res.code) {
-      //       // 发起网络请求
-      //       wx.request({
-      //         url: 'http://localhost:8080/ok',
-      //         data: {
-      //           code: res.code
-      //         }
-      //       })
-      //     } else {
-      //       console.log('登录失败！' + res.errMsg)
-      //     }
-      //   }
-      // })
-
-
-
+      $Message({
+        content: "头像信息获取成功",
+        type: 'success'
+      });
     }
   }
 })
