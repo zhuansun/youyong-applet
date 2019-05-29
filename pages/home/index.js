e7ece0cf6c6cfc1aef6014059bcb12e331fc90e8
颛孙鹏程 <zhuansunpengcheng@qq.com>
2019-05-29 17:29:13 +0800

    update ： 首页可以了

--- pages/home/index.js
+++ pages/home/index.js
@@ -7,20 +7,11 @@ Page({
     deviceHeight: null,
     class: 'success',
     visible: false,
-    data: {
-      'list': [{
-          'id': '1',
-          'title': '迅雷',
-          'color': 'success',
-          'content': '这里是迅雷描述'
-        },
-        {
-          'id': '2',
-          'title': '百度云',
-          'color': 'primary',
-          'content': '这里是百度云描述'
-        }
-      ]
+    dataList: [],
+    paginate: {
+      page: 1,
+      pageSize: 20,
+      hasNext: false
     }
   },
 
@@ -38,7 +29,7 @@ Page({
 
   onLoad: function() {
     this.getSystemInfo();
-    this.getModuleList();
+    this.getModuleList(this.data.paginate.page);
   },
 
   clickToUseFunction: function(e) {
@@ -67,97 +58,81 @@ Page({
   },
 
 
-  // 下拉刷新
-  onRefresh: function() {
-    // 显示顶部刷新图标
-    wx.showNavigationBarLoading();
-    console.log("下拉刷新");
-
-    // 隐藏导航栏加载框
-    wx.hideNavigationBarLoading();
-    // 停止下拉动作
-    wx.stopPullDownRefresh();
-
-
-    // var that = this;
-    // wx.request({
-    //   url: 'https://xxx/?page=0',
-    //   method: "GET",
-    //   header: {
-    //     'content-type': 'application/text'
-    //   },
-    //   success: function (res) {
-    //     that.setData({
-    //       moment: res.data.data
-    //     });
-    //     console.log(that.data.moment);
-    //     // 隐藏导航栏加载框
-    //     wx.hideNavigationBarLoading();
-    //     // 停止下拉动作
-    //     wx.stopPullDownRefresh();
-    //   }
-    // })
-  },
+  // // 下拉刷新
+  // onRefresh: function() {
+  //   // 显示顶部刷新图标
+  //   wx.showNavigationBarLoading();
+  //   console.log("下拉刷新");
+  //   this.getModuleList(1);
+  //   // 隐藏导航栏加载框
+  //   wx.hideNavigationBarLoading();
+  //   // 停止下拉动作
+  //   wx.stopPullDownRefresh();
+  // },
 
 
   /**
    * 页面上拉触底事件的处理函数
    */
   getMore: function() {
-    var that = this;
-    // 显示加载图标
-    wx.showLoading({
-      title: '玩命加载中',
-    })
-
-    console.log("下拉加载");
-    wx.hideLoading();
-    // // 页数+1
-    // page = page + 1;
-    // wx.request({
-    //   url: 'https://xxx/?page=' + page,
-    //   method: "GET",
-    //   // 请求头部
-    //   header: {
-    //     'content-type': 'application/text'
-    //   },
-    //   success: function (res) {
-    //     // 回调函数
-    //     var moment_list = that.data.moment;
-    //     const oldData = that.data.moment;
-    //     that.setData({
-    //       moment: oldData.concat(res.data.data)
-    //     })
-    //     // 隐藏加载框
-    //     wx.hideLoading();
-    //   }
-    // })
-
+    if(this.data.paginate.hasNext){
+      console.log("下拉加载，下拉加载下拉加载，下拉加载下拉加载，下拉加载下拉加载，下拉加载");
+      // 显示加载图标
+      // wx.showLoading({
+      //   title: '玩命加载中',
+      // })
+      this.getModuleList(this.data.paginate.page + 1);
+      // wx.hideLoading();
+    }
+    
   },
 
-
-
   //获取首页数据，进行加载
-  getModuleList: function() {
+  getModuleList: function(currentPage) {
     //获取token
     let token = wx.getStorageSync('token');
-    console.log(token);
+    console.log("---->"+currentPage);
+    const _this = this;
+    
     //将token放在请求头中。进行请求获取
     wx.request({
       url: app.urlConfig.basePath + '/api/home/list',
+      data: {
+        'page': currentPage,
+        'pageSize': this.data.paginate.pageSize,
+        'query':{}
+      },
       method: "POST",
       header: {
         'content-type': 'application/json', // 默认值
         'authorization': token
       },
       success(res) {
-        console.log(res.data.code);
         if (res.data.code == 200) {
           console.log("success");
+          if (currentPage === 1) {
+            _this.setData({
+              dataList: res.data.voList,
+              paginate: {
+                page: res.data.paginate.page,
+                pageSize: res.data.paginate.perPage,
+                hasNext: res.data.paginate.hasNext
+              }
+            })
+          }else{
+            _this.setData({
+              dataList: [..._this.data.dataList, ...res.data.voList],
+              paginate: {
+                page: res.data.paginate.page,
+                pageSize: res.data.paginate.perPage,
+                hasNext: res.data.paginate.hasNext
+              }
+            })
+            wx.hideLoading();
+          }
         } else if (res.data.code == 207 || res.data.code == 206) {
-          console.log(res.data.msg);
+          //失败
           app.globalData.errorMessage = res.data.msg;
-          //清除token
           wx.removeStorageSync("token");
           wx.redirectTo({
             url: '../index/index',
